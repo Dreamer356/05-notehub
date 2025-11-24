31661a534da1d36ed9fa44ca73f7f44103e8746a
Dreamer356 <glukhovsky.v@gmail.com>
2025-11-24 20:31:34 +0200

    +

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -8,6 +8,16 @@ interface ModalProps {
 }
 
 export const Modal = ({ onClose, children }: ModalProps) => {
+  useEffect(() => {
+    // Заблокировать прокрутку при монтировании
+    const originalStyle = window.getComputedStyle(document.body).overflow;
+    document.body.style.overflow = "hidden";
+
+    // Убрать блокировку при размонтировании
+    return () => {
+      document.body.style.overflow = originalStyle;
+    };
+  }, []);
 
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
@@ -21,13 +31,9 @@ export const Modal = ({ onClose, children }: ModalProps) => {
     return () => window.removeEventListener("keydown", handleKeyDown);
   }, [onClose]);
 
-
   return createPortal(
     <div className={css.backdrop} onClick={onClose}>
-      <div
-        className={css.modal}
-        onClick={(e) => e.stopPropagation()} 
-      >
+      <div className={css.modal} onClick={(e) => e.stopPropagation()}>
         {children}
       </div>
     </div>,
